6ec75394f9f53e3b3016eb917281cdc95169fcfe
Waghmarekranti <157002807+Waghmarekranti@users.noreply.github.com>
2024-03-13 14:35:17 +0530

    Create kranti.py

---
+++ kranti.py
@@ -0,0 +1,10 @@
+# Python Program to Add Two Numbers Without User Input
+
+num1 = 15
+num2 = 20
+
+# Addition of two numbers
+sum = num1 + num2
+
+# Display the sum of two numbers
+print('The sum of two number is {2}'.format(num1, num2, sum))
